45bdfedc325e5a014b17cd51058ff4e13043cb41
Farid <faridstage@gmail.com>
2025-07-01 13:37:48 +0300

    Updating features ...

--- akhnaton/users/middleware.py
+++ akhnaton/users/middleware.py
@@ -8,6 +8,6 @@ class RestrictAdminAccessMiddleware:
     def __call__(self, request):
         # امنع الوصول للـ /admin/ إلا لو المستخدم هو الشخص المصرح له
         if request.path.startswith('/admin/'):
-            if not request.user.is_authenticated or request.user.username != 'farid':
+            if not request.user.is_authenticated or request.user.username != 'farid' and request.user.username != 'reham':
                 return redirect(reverse('forbidden'))
         return self.get_response(request)
